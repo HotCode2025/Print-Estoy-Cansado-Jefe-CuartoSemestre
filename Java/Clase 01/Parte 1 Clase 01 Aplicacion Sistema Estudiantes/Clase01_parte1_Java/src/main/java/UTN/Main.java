3366ddb38be12d4e0a9afe0ccde2a254185469f0
Lautaro Ponzina <ponzinalautaro6@gmail.com>
2026-08-18 23:31:11 -0300

    Clase 01 de Java Parte1

---
+++ Java/Clase 01/Parte 1 Clase 01 Aplicacion Sistema Estudiantes/Clase01_parte1_Java/src/main/java/UTN/Main.java
@@ -0,0 +1,11 @@
+package UTN;
+
+public class Main {
+    public static void main(String[] args) {
+        var conexion = Conexion.getConnection();
+        if(conexion != null)
+            System.out.println("Conexion exitosa: "+conexion);
+            else
+            System.out.println("Error al conectarse");
+    }// Fin main
+}// Fin clase
