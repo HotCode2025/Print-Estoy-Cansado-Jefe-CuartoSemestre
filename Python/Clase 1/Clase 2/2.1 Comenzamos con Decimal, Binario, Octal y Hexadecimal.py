68eb1c5c66e331252cc2d71816995232cb231ff1
farreceygor <129985516+farreceygor@users.noreply.github.com>
2026-08-24 19:48:21 -0300

    Clase 2 Python Completa

---
+++ Python/Clase 1/Clase 2/2.1 Comenzamos con Decimal, Binario, Octal y Hexadecimal.py
@@ -0,0 +1,18 @@
+# Profundiozando en los sistemas de numeración
+
+# Sistema Decimal
+
+a = 10
+print(f"a decimal: {a}")
+
+# Sistema Binario
+a = 0b1010
+print(f"a binario: {a}")
+
+# Sistema Octal
+a = 0o12
+print(f"a octal: {a}")
+
+# Sistema Hexadecimal
+a = 0xA
+print(f"a hexadecimal: {a}")
