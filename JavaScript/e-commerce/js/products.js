917c12a88403ec14529802ad37a56c6345d51572
Thorffin <emirsat54@hotmail.com>
2026-08-24 19:58:22 -0300

    e-commerce

---
+++ JavaScript/e-commerce/js/products.js
@@ -0,0 +1,44 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 150,
+        quanty: 1,
+        img: "../media/ft.banana.png",
+    },
+    {
+        id: 2,
+        productName: "Leche",
+        price: 500,
+        quanty: 1,
+        img: "../media/ft.leche.png",
+    },
+    {
+        id: 3,
+        productName: "Pollo",
+        price: 1000,
+        quanty: 1,
+        img: "../media/ft.pollo.png",
+    },
+    {
+        id: 4,
+        productName: "Mayonesa",
+        price: 350,
+        quanty: 1,
+        img: "../media/ft.mayonesa.png",
+    },
+      {
+        id: 5,
+        productName: "enlatados",
+        price: 250,
+        quanty: 1,
+        img: "../media/ft.enlatados.png",
+    },
+       {
+        id: 5,
+        productName: "varios",
+        price: 2500,
+        quanty: 1,
+        img: "../media/ft.varios.png",
+    },
+]
