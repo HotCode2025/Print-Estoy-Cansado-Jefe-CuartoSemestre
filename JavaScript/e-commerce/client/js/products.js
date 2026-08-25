4a4535b491be3c71b3d69e3d32dc5e800751908b
Abril Cisterna <abril.cisterna23@gmail.com>
2026-08-24 21:39:59 -0300

    Reorganizacion de carpetas y modificaciones al codigo
    - Se reorganizaron las carpetas de la clase de e-commerce de Javascript.
    - Se modifico el codigo para que el mismo sea funcional y coherente a los contenidos vistos en clases.
    - Se elimino archivo vacio de inicializacion de la carpeta.

---
+++ JavaScript/e-commerce/client/js/products.js
@@ -0,0 +1,44 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 150,
+        quanty: 1,
+        img: "/e-commerce/client/media/ft.banana.png",
+    },
+    {
+        id: 2,
+        productName: "Leche",
+        price: 500,
+        quanty: 1,
+        img: "/e-commerce/client/media/ft.leche.png",
+    },
+    {
+        id: 3,
+        productName: "Pollo",
+        price: 1000,
+        quanty: 1,
+        img: "/e-commerce/client/media/ft.pollo.png",
+    },
+    {
+        id: 4,
+        productName: "Mayonesa",
+        price: 350,
+        quanty: 1,
+        img: "/e-commerce/client/media/ft.mayonesa.png",
+    },
+      {
+        id: 5,
+        productName: "enlatados",
+        price: 250,
+        quanty: 1,
+        img: "/e-commerce/client/media/ft.enlatados.png",
+    },
+       {
+        id: 6,
+        productName: "varios",
+        price: 2500,
+        quanty: 1,
+        img: "/e-commerce/client/media/ft.varios.png",
+    },
+]
