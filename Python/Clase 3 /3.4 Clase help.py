3f03bf7f3b854311dba94449b251cd0be8cb5f44
Martin0774 <martinplatero07@gmail.com>
2026-08-31 22:18:43 -0300

    Subo 3.4

---
+++ Python/Clase 3 /3.4 Clase help.py
@@ -0,0 +1,14 @@
+import math
+
+#Profundizando en el tipo String
+#Concatenacion automatica en Python
+
+variable = "Adios"
+mensaje = "Hola" "Alumnos" + variable
+mensaje += ", Terminamos"
+#print(mensaje)
+
+#Usamos la clase help para ayuda o documentación (built-in)
+
+#help(str)
+help(math.isnan)
