dda0aa9c2985684a4b0f6a93a4cc6aa020aa9aa8
Martin0774 <martinplatero07@gmail.com>
2026-08-31 22:11:31 -0300

    3.2 constructor bool completo

--- Python/Clase 3 /3.2 constructor bool().py
+++ Python/Clase 3 /3.2 constructor bool().py
@@ -7,3 +7,56 @@ print(f"valor: {valor}, Resultado: {resultado}")
 valor = 0.1
 resultado = bool(valor)
 print(f"valor: {valor}, Resultado: {resultado}")
+
+#Tipo string -> False "", True demás valores
+valor = ""
+resultado = bool(valor)
+print(f"valor: {valor}, Resultado: {resultado}")
+
+valor = "Hola"
+resultado = bool(valor)
+print(f"valor: {valor}, Resultado: {resultado}")
+
+#Tipo colecciones -> False para colecciones vacias
+#Tipo colecciones -> True para todas las demás
+
+#Lista
+valor = []
+resultado = bool(valor)
+print(f"valor: {valor}, Resultado: {resultado}")
+
+valor = [2, 3, 4]
+resultado = bool(valor)
+print(f"valor: {valor}, Resultado: {resultado}")
+
+#Tupla
+valor = ()
+resultado = bool(valor)
+print(f"valor: {valor}, Resultado: {resultado}")
+
+valor = (5,)
+resultado = bool(valor)
+print(f"valor: {valor}, Resultado: {resultado}")
+
+#Diccionario
+valor = {}
+resultado = bool(valor)
+print(f"valor: {valor}, Resultado: {resultado}")
+
+valor = {"Nombre": "Juan", "Apellido": "Perez"}
+resultado = bool(valor)
+print(f"valor: {valor}, Resultado: {resultado}")
+
+#Sentencias de control con bool
+if (1,):
+    print("Regresa verdadero")
+else:
+    print("Regresa falso")
+
+#ciclos
+variable = 17
+while variable:
+    print("Regresa verdadero")
+    break
+else:
+    print("Regresa falso")
