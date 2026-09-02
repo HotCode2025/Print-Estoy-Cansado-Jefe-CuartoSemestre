408f6989a0ac50477931295877a723385d3f5ab7
Abril Cisterna <abril.cisterna23@gmail.com>
2026-09-01 22:52:57 -0300

    Se agregan cambios de la clase 03

--- JavaScript/e-commerce/client/js/cart.js
+++ JavaScript/e-commerce/client/js/cart.js
@@ -4,29 +4,88 @@ const modalOverlay = document.getElementById("modal-overlay");
 const cartBtn = document.getElementById("cart-btn");
 
 const displayCart = () => {
-    modalContainer.innerHTML = "";
-    modalContainer.style.display = "block";
-    modalOverlay.style.display = "block";
+  modalContainer.innerHTML = "";
+  modalContainer.style.display = "block";
+  modalOverlay.style.display = "block";
 
-    //modal header
-    const modalHeader = document.createElement("div");
+  //modal header
+  const modalHeader = document.createElement("div");
 
-    const modalClose = document.createElement("div");
-    modalClose.innerText = "❌";
-    modalClose.className = "modal-close";
-    modalHeader.append(modalClose);
+  const modalClose = document.createElement("div");
+  modalClose.innerText = "❌";
+  modalClose.className = "modal-close";
+  modalHeader.append(modalClose);
 
-    modalClose.addEventListener("click", () => {
-        modalContainer.style.display = "none";
-        modalOverlay.style.display = "none";
-    })
+  modalClose.addEventListener("click", () => {
+    modalContainer.style.display = "none";
+    modalOverlay.style.display = "none";
+  });
 
-    const modalTitle = document.createElement("div");
-    modalTitle.innerText = "Cart";
-    modalTitle.className = "modal-title";
-    modalHeader.append(modalTitle);
+  const modalTitle = document.createElement("div");
+  modalTitle.innerText = "Cart";
+  modalTitle.className = "modal-title";
+  modalHeader.append(modalTitle);
 
-    modalContainer.append(modalHeader);
+  modalContainer.append(modalHeader);
+
+  //modal Body
+  cart.forEach((product) => {
+    const modalBody = document.createElement("div");
+    modalBody.className = "modal-body";
+    modalBody.innerHTML = `
+    <div class="product">
+            <img class-"product-img" src="${product.img}" />
+            <div class="product-info">
+                <h4>${product.productName}</h4>
+            </div>
+        <div class="quantity">
+            <span class="quantity-btn-decrease">-</span>
+            <span class="quantity-input">${product.quantity}</span>
+            <span class="quantity-btn-increase">+</span>
+        </div>
+            <div class="price">${product.price * product.quantity} $</div>
+            <div class="delete-product">❌</div>
+    </div>
+        `;
+    modalContainer.append(modalBody);
+
+    const decrease = modalBody.querySelector(".quantity-btn-decrease");
+    decrease.addEventListener("click", () => {
+      if (product.quantity !== 1) {
+        product.quantity--;
+        displayCart();
+      }
+    });
+
+    //Borrar elementos del carro
+    const deleteProduct = modalBody.querySelector(".delete-product");
+    deleteProduct.addEventListener("click", () => {
+      deleteCartProduct(product.id);
+    });
+
+    const increase = modalBody.querySelector(".quantity-btn-increase");
+    increase.addEventListener("click", () => {
+      product.quantity++;
+      displayCart();
+    });
+  });
+
+  //modal Footer
+  const total = cart.reduce((acc, el) => acc + el.price * el.quantity, 0);
+
+  const modalFooter = document.createElement("div");
+  modalFooter.className = "modal-footer";
+  modalFooter.innerHTML = `
+<div class ="total-price">Total: ${total} $</div>
+`;
+  modalContainer.append(modalFooter);
 };
 
 cartBtn.addEventListener("click", displayCart);
+
+//Borrar elementos del carro
+const deleteCartProduct = (id) => {
+  const foundId = cart.findIndex((element) => element.id === id);
+  cart.splice(foundId, 1);
+  displayCart();
+}
