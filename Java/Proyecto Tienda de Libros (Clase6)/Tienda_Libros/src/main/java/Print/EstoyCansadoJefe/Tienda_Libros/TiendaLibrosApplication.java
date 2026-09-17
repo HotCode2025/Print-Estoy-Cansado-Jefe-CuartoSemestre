22e0fa43e747715a271a2bed1f74515663411e61
RoyBruna <roybruna1580@gmail.com>
2026-09-16 21:44:03 -0300

    se importo el nuevo proyecto Venta_Libros

---
+++ Java/Proyecto Tienda de Libros (Clase6)/Tienda_Libros/src/main/java/Print/EstoyCansadoJefe/Tienda_Libros/TiendaLibrosApplication.java
@@ -0,0 +1,13 @@
+package Print.EstoyCansadoJefe.Tienda_Libros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaLibrosApplication.class, args);
+	}
+
+}
---
+++ Java/Proyecto Tienda de Libros (Clase6)/Tienda_Libros/src/test/java/Print/EstoyCansadoJefe/Tienda_Libros/TiendaLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package Print.EstoyCansadoJefe.Tienda_Libros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
